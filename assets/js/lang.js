e262095c994ec16571d7255398c48cc4ebd7e82f
arentarabrand <arentara.palmsugar@gmail.com>
2025-06-09 07:58:02 +0700

    Update lang.js

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,1 +1,18 @@
+async function setLanguage(lang) {
+  const res = await fetch(`lang/${lang}.json`);
+  const data = await res.json();
 
+  document.querySelectorAll("[data-i18n]").forEach((el) => {
+    const key = el.getAttribute("data-i18n");
+    if (data[key]) el.innerText = data[key];
+  });
+
+  localStorage.setItem("lang", lang);
+}
+
+// Tombol ganti bahasa (optional)
+document.addEventListener("click", (e) => {
+  if (e.target.matches("[data-set-lang]")) {
+    setLanguage(e.target.getAttribute("data-set-lang"));
+  }
+});
