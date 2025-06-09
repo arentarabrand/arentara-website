25a105eb57c750febf372a1b720ac1eafc40dc3e
arentarabrand <arentara.palmsugar@gmail.com>
2025-06-09 07:54:59 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+const components = ["header", "hero", "footer"]; // bisa tambah product-card dsb
 
+components.forEach(async (name) => {
+  const el = document.getElementById(name);
+  if (el) {
+    const res = await fetch(`components/${name}.html`);
+    const html = await res.text();
+    el.innerHTML = html;
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedLang = localStorage.getItem("lang") || "id";
+  setLanguage(savedLang);
+});
